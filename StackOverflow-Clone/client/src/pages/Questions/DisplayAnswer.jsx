1db767190fe1e1917960e28a3766555a08d1fb32
rimjhim-ag <rimjhim.agrawal_cs21@gla.ac.in>
2024-01-26 22:38:42 +0530

    ohk

--- StackOverflow-Clone/client/src/pages/Questions/DisplayAnswer.jsx
+++ StackOverflow-Clone/client/src/pages/Questions/DisplayAnswer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import withWeatherLogic from "../Weather";
 
 import Avatar from "../../components/Avatar/Avatar";
 import { deleteAnswer } from "../../actions/question";
@@ -57,4 +58,4 @@ const DisplayAnswer = ({ question, handleShare }) => {
   );
 };
 
-export default DisplayAnswer;
+export default withWeatherLogic(DisplayAnswer);
