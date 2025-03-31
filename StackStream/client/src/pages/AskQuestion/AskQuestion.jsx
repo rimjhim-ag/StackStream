9774f962635c09099db6f5bf47e8d23b5379c279
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-03-31 20:19:12 +0530

    Update AskQuestion.jsx

--- StackStream/client/src/pages/AskQuestion/AskQuestion.jsx
+++ StackStream/client/src/pages/AskQuestion/AskQuestion.jsx
@@ -1,7 +1,7 @@
 import React , {useState} from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import {useNavigate} from 'react-router-dom'
-import withWeatherLogic from "../Weather";
+
 
 import './AskQuestion.css'
 import { askQuestion } from "../../actions/question";
