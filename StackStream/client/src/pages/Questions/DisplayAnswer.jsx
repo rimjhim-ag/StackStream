e2b32f7383156a780945bb21e503ff64cf96730b
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-03-31 20:16:55 +0530

    Update DisplayAnswer.jsx

--- StackStream/client/src/pages/Questions/DisplayAnswer.jsx
+++ StackStream/client/src/pages/Questions/DisplayAnswer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import moment from "moment";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import withWeatherLogic from "../Weather";
+
 
 import Avatar from "../../components/Avatar/Avatar";
 import { deleteAnswer } from "../../actions/question";
