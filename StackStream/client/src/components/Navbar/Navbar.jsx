6e976c0fe4e35bc32913956d33ea7c408b4fcc80
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-03-31 18:58:27 +0530

    Update Navbar.jsx

--- StackStream/client/src/components/Navbar/Navbar.jsx
+++ StackStream/client/src/components/Navbar/Navbar.jsx
@@ -1,84 +1,83 @@
-import React , {useEffect} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
-import decode from 'jwt-decode'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import decode from "jwt-decode";
 
-import Avatar from '../../components/Avatar/Avatar'
-import logo from   '../../assets/logo.png'
-import search from '../../assets/search-solid.svg'
-import './Navbar.css'
+import logo from "../../assets/logo-stackoverflow.png";
+import search from "../../assets/icons8-search.svg";
+import Avtar from "../../Comps/Avtar/Avtar";
+import { setCurrentUser } from "../../actions/currentUser";
+import "./Nav.css";
 
-import { setCurrentUser } from '../../actions/currentUser'
-
-
-
-const Navbar = () => {
-  const navigate = useNavigate();
+const Nav = () => {
   const dispatch = useDispatch();
-
-
-  const handleLogout =()=>{
-    dispatch({type:'LOGOUT'})
-    navigate('/')
-    dispatch(setCurrentUser(null));
-  }
-
-
   var User = useSelector((state) => state.currentUserReducer);
-    
-  
-    useEffect(()=>{
-       const token = User?.token
-       if(token){
-         const decodeToken = decode(token)
-         if(decodeToken.exp * 1000 < new Date().getTime()){
-           handleLogout();
-         }
-       }
-         dispatch(setCurrentUser(JSON.parse(localStorage.getItem('Profile'))))
-    }, [dispatch])
-  
-  
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+    dispatch(setCurrentUser(null));
+  };
+
+  useEffect(() => {
+    const token = User?.token;
+    if (token) {
+      const decodeToken = decode(token);
+      if (decodeToken.exp * 1000 < new Date().getTime()) {
+        handleLogout();
+      }
+    }
+    dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
+  }, [User?.token, dispatch]);
+
   return (
-
-
-    <nav className='main-nav'>
-        <div className='navbar'>
-                <Link to='/' className ='nav-item nav-logo'>
-                <img src ={logo} alt='logo'   />
-
-                </Link>
-
-                <Link to='/' className='nav-item nav-btn'>
-                          About
-                </Link>
-                <Link to='/' className='nav-item nav-btn'>
-                       Products
-                </Link>
-                <Link to='/' className='nav-item nav-btn'>
-                             For Teams
-                </Link>
-                <form>
-                    <input type="text" placeholder='Search...'/>
-                    <img src={search} alt='search' width="18" className='search-icon'/>
-                </form>
-                 {User ==null ? 
-                
-                  <Link to="/Auth" className='nav-item nav-links'> Log In
-                   </Link>:
-                   <>
-                   <Avatar backgroundColor='#009dff' px="15px" py="10px" borderRadius="50%" color='white'><Link to={`/Users/${User?.result?._id}`} style={{color: "white" , textDecoration: 'none'}}>{User.result.name.charAt(0).toUpperCase()}</Link> </Avatar>
-                   <button className='nav-item nav-links' onClick={handleLogout} style={{marginLeft: '10px'}}>
-                      Log out
-                   </button>
-                  
-                   </>
-                
-                }
-              
+    <nav className="main-nav">
+      <div className="navbar">
+        <div className="navbar-1">
+          <Link to="/" className="nav-item nav-logo">
+            <img src={logo} alt="logo" width={130} />
+          </Link>
+          {/* <Link to="/" className="nav-item nav-btn">
+            About
+          </Link> */}
+          <form>
+            <input type="text" placeholder="Search..." />
+            <img src={search} alt="Search" width="18" className="srch-icn" />
+          </form>
         </div>
-    </nav>
-  )
-}
 
-export default Navbar
+        <div className="navbar-2">
+          {User === null ? (
+            <Link to="/Auth" className="nav-item nav-links">
+              Log in
+            </Link>
+          ) : (
+            <>
+              <Avtar
+                backgroundColor="#009dff"
+                px="10px"
+                py="6px"
+                borderRadius="50%"
+                color="white"
+              >
+                <Link
+                  to={`/Users/${User?.result?._id}`}
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  {User.result.name.charAt(0).toUpperCase()}
+                </Link>
+              </Avtar>
+
+              <button className="nav-item nav-links" onClick={handleLogout}>
+                Log out
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
